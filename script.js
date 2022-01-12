22916f6b3626e0ee94b134bcf1ef2c370d87002d
Nurtai <adyluulunurtai@gmail.com>
2022-01-12 09:57:17 +0300

    добавил условия и циклы

--- script.js
+++ script.js
@@ -7,9 +7,36 @@ let personalMovieDb = {
     'genres' : [],
     'privat' : false
 }
-let a = prompt("один из последних фиильмов" , '');
-let b = prompt("дайте оценку" , '');
 
-personalMovieDb.movies[a] = "ddd";
+
+
+for (let i = 0; i < 2 ; i++) {
+    let a = prompt("один из последних фиильмов" , '');
+    let b = prompt("дайте оценку" , '');
+    
+    if(a!= null && b!=null && a!='' && b!= '' && a.length<50){
+        personalMovieDb.movies[a] = b;
+        console.log("done")
+    } 
+    else{
+        i--
+    };
+    
+};
+if(personalMovieDb.count<10){
+    console.log("prosmotreno malo filmov")
+}
+else if (personalMovieDb.count>10){
+    console.log("klassi4eskii zritel")
+}
+else if ( personalMovieDb.count>30){
+console.log("kinoman")
+}
+else{
+    console.log('proizowla owibka')
+}
+
+
+
 
 console.log(personalMovieDb);
