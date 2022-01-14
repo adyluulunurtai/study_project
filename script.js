4d11a406df0bb7a5f0d334e7dc7282878195417e
Nurtai <adyluulunurtai@gmail.com>
2022-01-14 07:44:31 +0300

    mod writegenres function

--- script.js
+++ script.js
@@ -1,53 +1,47 @@
-
-
 let numberOfFilms;
 
-function start(){
+function start() {
     numberOfFilms = prompt("сколько фильмов вы уже посмотрели", '');
-    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms) ){
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
         numberOfFilms = prompt("сколько фильмов вы уже посмотрели", '');
     }
 }
 //start();
 
 let personalMovieDb = {
-    'count' : numberOfFilms,
-    'movies' : {},
-    'actors' : {},
-    'genres' : [],
-    'privat' : false
+    'count': numberOfFilms,
+    'movies': {},
+    'actors': {},
+    'genres': [],
+    'privat': false
 }
 
 
-function rememberMyFilms(){
-    for (let i = 0; i < 2 ; i++) {
-        let a = prompt("один из последних фиильмов" , '');
-        let b = prompt("дайте оценку" , '');
-        
-        if(a!= null && b!=null && a!='' && b!= '' && a.length<50){
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("один из последних фиильмов", '');
+        let b = prompt("дайте оценку", '');
+
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
             personalMovieDb.movies[a] = b;
             console.log("done")
-        } 
-        else{
+        } else {
             i--
         };
-        
+
     };
 };
 
 //rememberMyFilms();
 
-function detecPersonLevel(){
-    if(personalMovieDb.count<10){
+function detecPersonLevel() {
+    if (personalMovieDb.count < 10) {
         console.log("prosmotreno malo filmov")
-    }
-    else if (personalMovieDb.count>10){
+    } else if (personalMovieDb.count > 10) {
         console.log("klassi4eskii zritel")
-    }
-    else if ( personalMovieDb.count>30){
-    console.log("kinoman")
-    }
-    else{
+    } else if (personalMovieDb.count > 30) {
+        console.log("kinoman")
+    } else {
         console.log('proizowla owibka')
     }
 };
@@ -55,19 +49,18 @@ function detecPersonLevel(){
 //detecPersonLevel();
 
 
-function showMyDb(){
-    if(personalMovieDb.privat == false){
+function showMyDb() {
+    if (personalMovieDb.privat == false) {
         console.log(personalMovieDb);
     }
 }
 showMyDb();
 
-function writeGenres(){
-    for (let i = 0; i < 3 ; i++) {
-        let a = prompt(`ваш любимый жанр под номером ${i+1}` , '');
-        personalMovieDb.genres.push(a);
+function writeGenres() {
+    for (let i = 0; i < 3; i++) {
+
+        personalMovieDb.genres.push(prompt(`ваш любимый жанр под номером ${i+1}`, ''));
     }
-   
+
 }
 writeGenres();
-
