0c2c1da1c101b2cc8dc80fc4fb2e55489bf3772c
Nurtai <adyluulunurtai@gmail.com>
2022-01-16 14:35:09 +0300

    записали все функции внутрь обьекта как методы и доработали некоторые из них

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-let numberOfFilms;
 
-function start() {
-    numberOfFilms = prompt("сколько фильмов вы уже посмотрели", '');
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = prompt("сколько фильмов вы уже посмотрели", '');
-    }
-}
-//start();
+
 
 let personalMovieDb = {
     'count': 1,
@@ -15,155 +8,67 @@ let personalMovieDb = {
         'bg' : 'ss'
     },
     'actors': {},
-    'genres': [1, 2, 3],
-    'privat': false
-};
-
-
-for (key in personalMovieDb) {
-    if(typeof(personalMovieDb[key]) === 'object'){
-        for(i in personalMovieDb[key]){
-            //console.log(`${i} immet zna4enie ${personalMovieDb[key][i]}`);
+    'genres': [],
+    'privat': false,
+    'start' : function(){
+        personalMovieDb.count = prompt("сколько фильмов вы уже посмотрели", '');
+        while ( personalMovieDb.count == '' ||  personalMovieDb.count == null || isNaN( personalMovieDb.count)) {
+            personalMovieDb.count = prompt("сколько фильмов вы уже посмотрели", '');
         }
-    }
-    else{
-        //console.log(`${key} имеет значение ${personalMovieDb[key]}`);
-    }
-              
-}
-
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        let a = prompt("один из последних фиильмов", '');
-        let b = prompt("дайте оценку", '');
-
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDb.movies[a] = b;
-            console.log("done")
+    },
+    'rememberMyFilms' : function(){
+            for (let i = 0; i < 2; i++) {
+                let a = prompt("один из последних фиильмов", '');
+                let b = prompt("дайте оценку", '');
+        
+                if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                    personalMovieDb.movies[a] = b;
+                    console.log("done")
+                } else {
+                    i--;
+                }
+            }
+    },
+    'detecPersonLevel' : function(){
+        if (personalMovieDb.count < 10) {
+            console.log("prosmotreno malo filmov");
+        } else if (personalMovieDb.count > 10) {
+            console.log("klassi4eskii zritel");
+        } else if (personalMovieDb.count > 30) {
+            console.log("kinoman");
         } else {
-            i--;
+            console.log('proizowla owibka');
         }
+    },
+    'showMyDb' : function(){
+        if (personalMovieDb.privat == false) {
+            console.log(personalMovieDb);
+        }
+    },
+    'writeGenres' : function(){
+        for (let i = 0; i < 3; i++) {
 
-    }
-}
-
-//rememberMyFilms();
-
-function detecPersonLevel() {
-    if (personalMovieDb.count < 10) {
-        console.log("prosmotreno malo filmov");
-    } else if (personalMovieDb.count > 10) {
-        console.log("klassi4eskii zritel");
-    } else if (personalMovieDb.count > 30) {
-        console.log("kinoman");
-    } else {
-        console.log('proizowla owibka');
+            let a = prompt(`ваш любимый жанр под номером ${i+1}`, '');
+            if (a === "" ||a ==null){
+                i--;
+            }
+            else{
+                personalMovieDb.genres.push(a);
+            }   
+        }
+        personalMovieDb.genres.forEach((item, i) => {
+            console.log(`любимый жанр № ${i+1} eto ${item}`)
+        }  );
+       
+    },
+    'toggleVisibleMyDb': function(){
+        if(personalMovieDb.privat){
+            personalMovieDb.privat = false;
+        }
+        else {
+            personalMovieDb.privat = true;
+        }
     }
 };
+personalMovieDb.start();
 
-//detecPersonLevel();
-
-
-function showMyDb() {
-    if (personalMovieDb.privat == false) {
-        console.log(personalMovieDb);
-    }
-}
-showMyDb();
-
-function writeGenres() {
-    for (let i = 0; i < 3; i++) {
-
-        personalMovieDb.genres.push(prompt(`ваш любимый жанр под номером ${i+1}`, ''));
-    }
-
-}
-//writeGenres();
-
-function learnJS(lang, callback){
-    console.log(`ya u4u ${lang}`);
-    callback();
-}
-
-//learnJS("javascript", function(){
-//    console.log("eto moi pervyi urok")
-//});
-
-let arr = [1, 2, 3, 9];
-
-for (let value of arr){
-    //console.log(value)
-}
- arr.forEach(function(element, i, array) {
-  console.log(`index ${i} imeet zna4enie ${element} v massive ${array} `)   
- });
-
- console.log(arr.length);
-
-
- arr.push(1, 24, 32, 43, 15, 26, 7, 8);
-
-
- arr.sort(compareNum);
-
- function compareNum(a, b){
-     return(a-b)
- }
-
- console.log(arr);
-
- let a = prompt('', '');
- let b = a.split(" ");
- b.join(' ')
- console.log(b);
-
-
- const obj = {
-     a: 1,
-     b: 3,
-     c: {
-         x: 7,
-         y: 10
-     }
- }
-
-
- function copyObj(mainObj){
-     let newObj = {};
-    
-     for(let value in mainObj){
-        newObj[value] = mainObj[value] ;
-     }
-     return (newObj);
- };
-
- let d = copyObj(obj);
- d.a = 12;
- d.c.x = 10;
- 
- let v = Object.assign({}, obj);
- v.a = 19;
-
- console.log(obj);
- //console.log(d);
- console.log(v);
-
- const oldArray = [1, 2, 3];
- const newArray = oldArray.slice();
-
- newArray[1] = 3;
-
- console.log(oldArray);
- console.log(newArray);
-
- const t = [1, 2, 3];
- const o = [...t];
- console.log (t);
- console.log(o)
-
- const video = ['youtube', 'vimeo', 'rutube'];
- const blogs = ['livejournal', 'wordpress' ];
- const internet = [...video, ...blogs, "vk", "instagram"]
-
- console.log(internet);
